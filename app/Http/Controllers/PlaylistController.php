e8f8e1a014d5ef898850b1b67ffd7a4099e3eff2
toniRamos <antonioramostorres@gmail.com>
2019-04-17 12:41:56 +0200

    Adding models, adding endpoing saved json to database

--- app/Http/Controllers/PlaylistController.php
+++ app/Http/Controllers/PlaylistController.php
@@ -2,22 +2,39 @@
 
 namespace App\Http\Controllers;
 
-use \DB;
 use Log;
 use Illuminate\Http\Request;
+use App\Services\ParsePlayList;
 
 class PlaylistController extends Controller{
+    private $parsePlayList;
 
     function __construct(){
-        
+        $this->parsePlayList = new ParsePlayList();
     }
 
     public function loadPlayList(Request $request){
+        $code = 200;
+        $message = "";
+
         $data = json_decode($request->getContent(), true);
 
-        print_r($data);die;
+        $resultSave = $this->parsePlayList->parsePlayList($data);
 
-        return "pong";
+        if($resultSave['totalPlayListSaved'] == 0){
+            $code = 500;
+            $message = "No playlist could be saved";
+        }
+
+        if($resultSave['totalPlayListSaved'] == $resultSave['totalPlayList']){
+            $message = "All lists could be saved";
+        }
+        
+        if($resultSave['totalPlayListSaved'] != $resultSave['totalPlayList']){
+            $message = "Some list could not be saved";
+        }
+
+        return response()->json($message, $code);
     }
 
     
